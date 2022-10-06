9643e80da255dac35b83a1658c06ea790aebed5d
joong-main <leeji88460@gmail.com>
2022-10-06 01:29:23 +0000

    2022/10/06 00

---
+++ posts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    content = models.TextField()
+    view_count = models.IntegerField(default=0)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('posts:show',args=[self.pk])
